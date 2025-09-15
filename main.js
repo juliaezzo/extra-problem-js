893d865661468333c9167d65e17e4827408a2d19
juliaezzo <ezzojulia5@gmail.com>
2025-09-15 17:10:33 +0300

    add1

---
+++ main.js
@@ -0,0 +1,9 @@
+// 1:reversee a string
+// const reverseString = (str) => {
+//     let newStr = "";
+    
+//     for(let i = str.length-1; i >= 0; i--) {
+//         newStr += str[i];
+//     }
+//     return newStr;
+// } 
