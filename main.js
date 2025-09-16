20bdd0bd7b44c44c665ec11710b327fb337a4d73
juliaezzo <ezzojulia5@gmail.com>
2025-09-16 05:53:03 +0300

    add3

--- main.js
+++ main.js
@@ -1,17 +1,14 @@
 // 1:reversee a string
 // const reverseString = (str) => {
 //     let newStr = "";
-    
+
 //     for(let i = str.length-1; i >= 0; i--) {
 //         newStr += str[i];
 //     }
 //     return newStr;
-// } 
+// }
 
-
-
-
-// 2:Remove duplicates from sorted array 
+// 2:Remove duplicates from sorted array
 //  const arr = [2 , 9 , 3 , 2 , 8];
 //  const newArr = [];
 
@@ -24,9 +21,28 @@
 //             break;
 //         }
 //     }
-       
+
 //     if(!isRepeated) {
 //       newArr[newArr.length] = arr[i];
 //     }
 //  }
 // console.log(newArr);
+
+// 3:Three Sum
+// const arr = [1, 4, 45, 6, 10, 8];
+// let sum = 13;
+
+// const hasTriplet = (arr, sum) => {
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = i + 1; j < arr.length; j++) {
+//       for (let k = j + 1; k < arr.length; k++) {
+//         let triplete = arr[i] + arr[j] + arr[k];
+
+//         if (triplete === sum) {
+//           return true;
+//         }
+//       }
+//     }
+//   }
+//   return false;
+// };
