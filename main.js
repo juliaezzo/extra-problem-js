cc6c6b8aeb69f695628bd5ce6099c7b0c949713a
juliaezzo <ezzojulia5@gmail.com>
2025-09-16 05:20:11 +0300

    add2

--- main.js
+++ main.js
@@ -7,3 +7,26 @@
 //     }
 //     return newStr;
 // } 
+
+
+
+
+// 2:Remove duplicates from sorted array 
+//  const arr = [2 , 9 , 3 , 2 , 8];
+//  const newArr = [];
+
+//  for(let i = 0; i < arr.length; i++) {
+//     let isRepeated = false;
+
+//     for(let j = 0; j < newArr.length; j++) {
+//         if(arr[i] === newArr[j]) {
+//             isRepeated = true;
+//             break;
+//         }
+//     }
+       
+//     if(!isRepeated) {
+//       newArr[newArr.length] = arr[i];
+//     }
+//  }
+// console.log(newArr);
